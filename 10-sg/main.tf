4675ec77ed01c350e5a1cc3c37c8d54244ea3569
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-05-02 12:32:49 +0530

    Roboshop Infra Terraform

---
+++ 10-sg/main.tf
@@ -0,0 +1,9 @@
+module "vote_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "${local.common_name_suffix}-catalogue"
+  use_name_prefix = false
+  description = "Security group for catalogue service with custom ports open within VPC, egress all ports"
+  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+
+}
