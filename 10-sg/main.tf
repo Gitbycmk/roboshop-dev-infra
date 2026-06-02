1e7504d94ccf18137d628ada4e6c7d465b25362b
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-06-02 14:16:49 +0530

    Terraform Roboshop Infra

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,9 +1,20 @@
-module "catalogue" {
-  source = "terraform-aws-modules/security-group/aws"
+## SG Creation Using Open Source Module
+# module "catalogue" {
+#   source = "terraform-aws-modules/security-group/aws"
 
-  name = "${local.common_name_suffix}-catalogue"
-  description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
-  vpc_id = data.aws_ssm_parameter.vpc_id.value
-  use_name_prefix = false
+#   name = "${local.common_name_suffix}-catalogue"
+#   description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
+#   vpc_id = data.aws_ssm_parameter.vpc_id.value
+#   use_name_prefix = false
 
+# }
+
+module "sg" {
+    count = length(var.sg_names)
+  source = "git::https://github.com/Gitbycmk/terraform-aws-sg.git?ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  sg_name = var.sg_names[count.index]
+  sg_description = "created for ${var.sg_names[count.index]}"
+  vpc_id = local.vpc_id
 }
