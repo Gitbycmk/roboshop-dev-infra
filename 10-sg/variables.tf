0980736f3c3a89aa2a439b7d6135e418ceed5a3f
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-06-03 11:07:06 +0530

    Terraform Roboshop Infra

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -15,6 +15,8 @@ variable "sg_names" {
         # frontend
         "frontend",
         # bastion
-        "bastion"
+        "bastion",
+        # frontend load balancer
+        "frontend-lb"
     ]
 }
