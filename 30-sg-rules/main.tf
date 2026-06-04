ff948b5e9a0dba81d1d31674b1de681a50b43b64
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-06-04 19:45:08 +0530

    Terraform Roboshop Infra

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -16,9 +16,21 @@ resource "aws_security_group_rule" "bastion_laptop" {
   to_port           = 22
 }
 
+# Mongodb accepting connections from bastion on port 22
 resource "aws_security_group_rule" "mongodb_" {
   type              = "ingress"
   security_group_id = local.mongodb_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
+
+
+# Redis accepting connections from bastion on port 22
+resource "aws_security_group_rule" "redis_bastion" {
+  type              = "ingress"
+  security_group_id = local.redis_sg_id
   source_security_group_id = local.bastion_sg_id
   from_port         = 22
   protocol          = "tcp"
--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/Gitbycmk/ansible-roboshop-roles-tf-main.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/Gitbycmk/ansible-roboshop-roles-tf-main.git -e component=$component main.yaml
