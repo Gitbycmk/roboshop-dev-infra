a097e3fc6428abb434ae71253bc6d02b54807a39
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-06-05 12:53:39 +0530

    Terraform Roboshop Infra

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 component=$1
+component=$2
 dnf install ansible -y
 # ansible-pull -U https://github.com/Gitbycmk/ansible-roboshop-roles-tf-main.git -e component=$component main.yaml
 
